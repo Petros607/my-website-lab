f4019b5fa2b1786b477b71c822bb1e03c146e182
Petros607 <markaryan.petr@mail.ru>
2026-03-01 00:07:07 +0400

    Изменил форму регистрации

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -54,7 +54,7 @@ function validateForm() {
     if (!/\d/.test(password))
         errors.push("Пароль должен содержать хотя бы одну цифру");
 
-    if (!/[!@#$%^&*]/.test(password))
+    if (!/[!@#$%^&_*]/.test(password))
         errors.push("Пароль должен содержать специальный символ");
 
     if (password !== confirmPassword)
