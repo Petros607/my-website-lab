119d449ab63d519da2f067d5500c060a98f993a6
Petros607 <markaryan.petr@mail.ru>
2026-03-01 02:18:54 +0400

    Refactor

--- frontend/js/register.js
+++ frontend/js/register.js
@@ -255,7 +255,7 @@ function validateForm() {
     if (!/\d/.test(password))
         errors.push("Пароль должен содержать хотя бы одну цифру");
 
-    if (!/[!@#$%^&*-]/.test(password))
+    if (!/[!@#$%^&*-_]/.test(password))
         errors.push("Пароль должен содержать специальный символ");
 
     if (password !== confirmPassword)
